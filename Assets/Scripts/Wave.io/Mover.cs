b12d19e92b714b5b7aa2f8a82f48d0144c952687
Minwoo Kim <minwook1130@gmail.com>
2023-09-13 16:01:14 -0400

    Basic movement scripts in progress

--- Assets/Scripts/Wave.io/Mover.cs
+++ Assets/Scripts/Wave.io/Mover.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private float degree;
+
+    private float delta = 50;
 
     void Update()
     {
-        
+        transform.position = new Vector3(Mathf.Sin(degree), Mathf.Cos(degree), 0) * delta * Time.deltaTime;
     }
 }
