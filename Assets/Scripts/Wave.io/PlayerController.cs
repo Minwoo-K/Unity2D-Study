70dfd0cd1e75a65648a173622c52797ef9313d0d
Minwoo Kim <minwook1130@gmail.com>
2023-09-14 15:58:10 -0400

    Added Player & Camera Controller scripts

--- Assets/Scripts/Wave.io/PlayerController.cs
+++ Assets/Scripts/Wave.io/PlayerController.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private PlayerMovement playerMovement;  // Player Movement script
+
     void Start()
     {
-        
+        // Fetch the component
+        playerMovement = GetComponent<PlayerMovement>();
     }
 
-    void Update()
+    // Mathematic calculation should be in FixedUpdate
+    void FixedUpdate()
     {
+        // Player always moves within the X axis
+        playerMovement.MoveInX();
         
+        // Moves up only when the left mouse button is being held down
+        if ( Input.GetMouseButton(0) )
+        {
+            playerMovement.MoveUpY();
+        }
     }
 }
--- Assets/Scripts/Wave.io/PlayerMovement.cs
+++ Assets/Scripts/Wave.io/PlayerMovement.cs
@@ -6,32 +6,35 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header("X axis Movement")]
     [SerializeField]
-    private float xSpeed;
+    private float xSpeed;           // Speed for X axis
     [SerializeField]
-    private float xRange;
+    private float xRange;           // The Range in X axis to move
 
-    private Vector3 startPosition;
+    private Vector3 startPosition;  // The Starting Position
 
     [Header("Y axis Movement")]
     [SerializeField]
-    private float ySpeed;
-    [SerializeField]
-    private float yRange;
+    private float ySpeed;           // Speed for Y axis
+    private Rigidbody2D rigidBody;  // Rigidbody to use AddForce
 
     private void Awake()
     {
-        startPosition = transform.position;
+        startPosition = transform.position;     // Initialize by saving the starting position
+        rigidBody = GetComponent<Rigidbody2D>();// Fetch the Rigidbody2D component
     }
 
     public void MoveInX()
     {
+        // Time.time always increases over time that makes the Sin value to change from 1 ~ -1
+        // Multiplying by xRange expands the max & min values
         float x = startPosition.x + xRange * Mathf.Sin(xSpeed * Time.time);
+        // Set the x value
         transform.position = new Vector3(x, transform.position.y, transform.position.z);
     }
 
-    public void MoveInY()
+    public void MoveUpY()
     {
-        float y = startPosition.y + yRange * Mathf.Cos(ySpeed * Time.time);
-        transform.position = new Vector3(transform.position.x, y, transform.position.z);
+        // Add force onto the rigidbody for the Position to move up
+        rigidBody.AddForce(transform.up * ySpeed, ForceMode2D.Impulse);
     }
 }
