82d165ef3e32f5799976a8f8341f337047db954f
Minwoo Kim <minwook1130@gmail.com>
2023-09-19 15:42:55 -0400

    Created Camera Controller

---
+++ Assets/Scripts/Wave.io/CameraController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Waveio
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform target;
+        [SerializeField]
+        private Vector3 captureOffset = new Vector3(0, 8, -10);
+        [SerializeField]
+        private float smoothTime;
+
+        private Vector3 velocity = Vector3.zero;
+
+        private void Update()
+        {
+            // To get a position value for the camera, BASED ON the target's position
+            // -> target is (standard) | TransformPoint(captureOffset) is the local position for camera with the offset position
+            Vector3 capturePosition = target.TransformPoint(captureOffset);
+            capturePosition.x = 0;
+            transform.position = Vector3.SmoothDamp(transform.position, capturePosition, ref velocity, smoothTime);
+        }
+    }
+}
