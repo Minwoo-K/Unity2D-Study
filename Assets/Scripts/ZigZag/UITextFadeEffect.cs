c10292130123ff6f96ebd592235c0609ee3183ac
Minwoo Kim <minwook1130@gmail.com>
2023-10-11 16:54:17 -0400

    Added UITextFadeEffect script

---
+++ Assets/Scripts/ZigZag/UITextFadeEffect.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace ZigZag
+{
+    public class UITextFadeEffect : MonoBehaviour
+    {
+        [SerializeField]
+        private float fadeTime;
+
+        private TextMeshProUGUI tmo;
+        private float alpha;
+        private AnimationCurve curve;
+
+
+        private void Awake()
+        {
+            tmo = GetComponent<TextMeshProUGUI>();
+            alpha = tmo.color.a;
+        }
+
+        public void StartFading()
+        {
+            StartCoroutine(FadeIn(0, alpha));
+        }
+
+        private IEnumerator FadeIn(float start, float end)
+        {
+            float current = 0;
+            float percent = 0;
+
+            while ( percent < 1 )
+            {
+                current += Time.deltaTime;
+                percent = current / fadeTime;
+
+                Color color = tmo.color;
+                color.a = curve.Evaluate(percent);
+                tmo.color = color;
+
+                yield return null;
+            }
+        }
+    }
+}
