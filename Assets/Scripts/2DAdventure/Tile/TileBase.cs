b5fa65f02f0430d7ee7a0aa53560255bff5ffa0f
Minwoo Kim <minwook1130@gmail.com>
2024-01-31 18:50:54 -0500

    Updated TileBase
    - Added Bouncing effect on Tile when bounceable

--- Assets/Scripts/2DAdventure/Tile/TileBase.cs
+++ Assets/Scripts/2DAdventure/Tile/TileBase.cs
@@ -4,8 +4,52 @@ using UnityEngine;
 
 public class TileBase : MonoBehaviour
 {
+    [SerializeField]
+    private bool bounceable = false;
+
+    private Vector3 startPosition;
+    private float bouncingAmount = 0.35f;
+
+    public bool isBouncing { private set; get; } = false;
+
+    private void Awake()
+    {
+        startPosition = transform.position;
+    }
+
     public virtual void UpdateCollsion()
     {
-        Debug.Log($"Collide with {gameObject.name}!");
+        if ( bounceable )
+        {
+            isBouncing = true;
+
+            StartCoroutine( OnBounce() );
+        }
+    }
+
+    public IEnumerator OnBounce()
+    {
+        yield return StartCoroutine(MoveInY(startPosition.y, startPosition.y + bouncingAmount));
+
+        yield return StartCoroutine(MoveInY(startPosition.y + bouncingAmount, startPosition.y ));
+
+        isBouncing = false;
+    }
+
+    private IEnumerator MoveInY(float startY, float endY)
+    {
+        float movingTime = 0.2f;
+        float percent = 0;
+
+        while ( percent < 1 )
+        {
+            percent += Time.deltaTime / movingTime;
+
+            Vector3 position = transform.position;
+            position.y = Mathf.Lerp(startY, endY, percent);
+            transform.position = position;
+
+            yield return null;
+        }
     }
 }
