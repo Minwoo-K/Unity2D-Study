3ea7efc2a382b5cdfc7ce6b8b6a4b66cd9c94364
Minwoo Kim <minwook1130@gmail.com>
2024-02-02 18:04:48 -0500

    Added PlatformDrop script

--- Assets/Scripts/2DAdventure/Platform/PlatformBase.cs
+++ Assets/Scripts/2DAdventure/Platform/PlatformBase.cs
@@ -6,7 +6,7 @@ namespace Adventure_2D
 {
     public abstract class PlatformBase : MonoBehaviour
     {
-        public bool IsHit { private set; get; } = false;
+        public bool IsHit { protected set; get; } = false;
 
         public abstract void UpdateCollision(GameObject other);
     }
