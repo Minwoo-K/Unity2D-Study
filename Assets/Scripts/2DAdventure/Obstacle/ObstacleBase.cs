f32848af5e67f8fcbbb45565542c2743cb510a3d
Minwoo Kim <minwook1130@gmail.com>
2024-02-06 15:27:46 -0500

    Added ObstacleBase
    - and Thorn objects are obstacles

---
+++ Assets/Scripts/2DAdventure/Obstacle/ObstacleBase.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleBase : MonoBehaviour
+{
+    [SerializeField]
+    protected bool isInstantDeath = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if ( collision.CompareTag("Player") == false ) return;
+
+        if ( isInstantDeath )
+        {
+            Debug.Log("Player Dead");
+        }
+        else
+        {
+            Debug.Log("Player Health Reduced");
+        }
+    }
+}
