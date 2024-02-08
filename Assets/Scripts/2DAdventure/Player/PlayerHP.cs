43a7a85df393cc2b6a53913f42cff27be5135fd4
Minwoo Kim <minwook1130@gmail.com>
2024-02-08 14:36:03 -0500

    Added Invincibility feature

--- Assets/Scripts/2DAdventure/Player/PlayerHP.cs
+++ Assets/Scripts/2DAdventure/Player/PlayerHP.cs
@@ -9,18 +9,67 @@ public class PlayerHP : MonoBehaviour
     [SerializeField]
     private int current;
 
+    private SpriteRenderer spriteRenderer;
+    private Color          originalColor;
+
+    private bool isInvincible = false;
+    private float invinciblilityTime;
+
     private void Awake()
     {
         current = max;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        originalColor = spriteRenderer.color;
     }
 
     public void DecreaseHP()
     {
+        if ( isInvincible ) return;
+
+        // when damaged, make Player invincible for 1 second
+        TurnOnInvincible(1);
+
         current --;
 
         if ( current == 0 )
         {
             Debug.Log("Player Dead");
         }
+    }
+
+    public void TurnOnInvincible(float timeFrame)
+    {
+        // Invincibility Overlapping situation
+        if ( isInvincible )
+        {
+            invinciblilityTime += timeFrame;
+        }
+        // when just got an invincibility
+        else
+        {
+            StartCoroutine(OnInvincible(timeFrame));
+        }
+    }
+
+    private IEnumerator OnInvincible(float timeFrame)
+    {
+        isInvincible = true;
+        invinciblilityTime = timeFrame;
+
+        float blinkSpeed = 10f;
+
+        while( invinciblilityTime > 0 )
+        {
+            invinciblilityTime -= Time.deltaTime;
+            Color color = spriteRenderer.color;
+
+            color.a = Mathf.SmoothStep(0, 1, Mathf.PingPong(Time.time * blinkSpeed, 1));
+            spriteRenderer.color = color;
+
+            yield return null;
+        }
+
+        spriteRenderer.color = originalColor;
+        isInvincible = false;
     }
 }
