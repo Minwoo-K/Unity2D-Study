7f08ab91009407cdf188d353199cdb309a4113f4
Minwoo Kim <minwook1130@gmail.com>
2024-02-09 16:16:32 -0500

    Prefabbed the PlayerProjectile

---
+++ Assets/Scripts/2DAdventure/Player/PlayerProjectile.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Adventure_2D
+{
+    public class PlayerProjectile : MonoBehaviour
+    {
+        private MovementRigidbody2D movement;   // To initially make the Projectile move
+        private float originalSpeedX;            // Original Speed to track of in X axis
+
+
+        public void Setup(int direction)
+        {
+            movement = GetComponent<MovementRigidbody2D>();
+            movement.MoveTo(direction);
+
+            originalSpeedX = Mathf.Abs(movement.Velocity.x);
+        }
+
+        private void Update()
+        {
+            // If Projectile hits ground, it jumps
+            if ( movement.IsOnGround ) movement.Jump();
+            // If the Projectile's velocity reduces, it's assumed that it's collided with something(that's not a ground-kind)
+            if ( Mathf.Abs(movement.Velocity.x) < originalSpeedX )
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
+
+
