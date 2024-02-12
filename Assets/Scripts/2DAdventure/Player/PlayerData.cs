12b8cc579534e5f998c30fab18f993b297a3ad56
Minwoo Kim <minwook1130@gmail.com>
2024-02-12 17:47:55 -0500

    Added Star Item feature

--- Assets/Scripts/2DAdventure/Player/PlayerData.cs
+++ Assets/Scripts/2DAdventure/Player/PlayerData.cs
@@ -8,6 +8,8 @@ namespace Adventure_2D
     {
         private int coin;
         private int projectile = 0;
+        [SerializeField]
+        private bool[] stars = new bool[3] { false, false, false };
 
         public int Coin
         {
@@ -20,6 +22,11 @@ namespace Adventure_2D
         {
             set => projectile = Mathf.Clamp(value, 0, MaxProjectile);
             get => projectile;
+        }
+
+        public void GrabStar(int index)
+        {
+            stars[index] = true;
         }
     }
 }
