d649bd40b28e68dc7effc14ded36034384e82eb5
Minwoo Kim <minwook1130@gmail.com>
2024-02-17 17:27:50 -0500

    Rearranged scripts
    - Moved MovementRigidbody2D to Common folrder
    - Added EnemyFlower - related scripts

--- Assets/Scripts/2DAdventure/Enemies/EnemyMushroomAnimator.cs
+++ Assets/Scripts/2DAdventure/Enemies/EnemyMushroomAnimator.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyAnimatorEvent : MonoBehaviour
+public class EnemyMushroomAnimator : MonoBehaviour
 {
     [SerializeField]
     private GameObject parent;
