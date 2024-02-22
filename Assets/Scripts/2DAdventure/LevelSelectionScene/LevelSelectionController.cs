ecac7e5bcc6bd4ae9d28ef1553b2a39f2ddd3737
Minwoo Kim <minwook1130@gmail.com>
2024-02-22 17:26:46 -0500

    Added a method to Define script
    - That loads level data according to a needed level

---
+++ Assets/Scripts/2DAdventure/LevelSelectionScene/LevelSelectionController.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelectionController : MonoBehaviour
+{
+    [SerializeField]
+    private Image fadingScreen;
+}
