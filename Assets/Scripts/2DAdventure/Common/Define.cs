2c399ca63cba0ec67ea8610766b8ec31c19cd1e3
Minwoo Kim <minwook1130@gmail.com>
2024-02-22 16:37:39 -0500

    Created UILevel script
    - To manage Levels in LevelSelection scene

--- Assets/Scripts/2DAdventure/Common/Define.cs
+++ Assets/Scripts/2DAdventure/Common/Define.cs
@@ -1,3 +1,7 @@
 public enum ItemType { Random=-1, Coin=0, Invincibility, Projectile, HPPotion, Star, Count }
 
+public class Define
+{
+    public static readonly string CurrentLevel = "CURRENT_LEVEL";
 
+}
