ce0e0eb637a17a21ed2faf1b02bd4b20abe6a84b
Minwoo Kim <minwook1130@gmail.com>
2024-03-15 19:41:34 -0400

    Implemented Platform_Moving

--- Assets/Scripts/2DAdventure/Platform/Platform_Moving.cs
+++ Assets/Scripts/2DAdventure/Platform/Platform_Moving.cs
@@ -62,4 +62,14 @@ public class Platform_Moving : MonoBehaviour
             yield return null;
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if ( collision.collider.CompareTag("Player") ) collision.transform.SetParent(targetPlatform);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player")) collision.transform.SetParent(null);
+    }
 }
