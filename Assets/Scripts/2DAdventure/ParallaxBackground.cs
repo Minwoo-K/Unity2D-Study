f93b947004c81e6f083a47d99b5e25764ce0a570
Minwoo Kim <minwook1130@gmail.com>
2024-04-02 16:09:47 -0400

    Implemented ParallaxBackground

--- Assets/Scripts/2DAdventure/ParallaxBackground.cs
+++ Assets/Scripts/2DAdventure/ParallaxBackground.cs
@@ -16,12 +16,18 @@ public class ParallaxBackground : MonoBehaviour
     private BackgroundData      backgroundCloud;
     [SerializeField]
     private BackgroundData[]    backgrounds;
-    [SerializeField]
-    private float               basicSpeed;
 
     private void Update()
     {
+        // Background Cloud: moves continously
         float offsetX = Time.time * backgroundCloud.offsetSpeed;
         backgroundCloud.background.material.mainTextureOffset = new Vector2(offsetX, 0);
+
+        // Other Backgrounds: moves based on the targetted Camera's movement
+        for ( int i = 0; i < backgrounds.Length; i++ )
+        {
+            float x = targetCamera.position.x * backgrounds[i].offsetSpeed;
+            backgrounds[i].background.material.mainTextureOffset = new Vector2(x, 0);
+        }
     }
 }
