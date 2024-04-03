df5dd0391f0c2a66d96f857930699cfdb6f0d780
Minwoo Kim <minwook1130@gmail.com>
2024-04-03 13:23:07 -0400

    Updated Tile_Base component for Bouncing effect
    * when using more than 1 Coroutine in a function, the function should return IEnumerator. Otherwise, the Coroutine won't be executed in the order but, altogether at a time.

--- Assets/Scripts/2DAdventure/Tile/Tile_Base.cs
+++ Assets/Scripts/2DAdventure/Tile/Tile_Base.cs
@@ -2,14 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tile_Base : MonoBehaviour
+public abstract class Tile_Base : MonoBehaviour
 {
-    public bool IsHit { get; protected set; } = false;
+    [Header("Tile_Base Component")]
+    [SerializeField]
+    protected bool bounceable = false;
+
+    // Properties
+    public bool IsBouncing  { get; protected set; } = false;
+    public bool IsHit       { get; protected set; } = false;
+
+    private void Awake()
+    {
+        
+    }
 
     public virtual void UponCollision(GameObject player)
     {
-        if ( IsHit ) return;
+        if (IsHit) return;
 
-        IsHit = true;
+        else
+        { 
+            IsHit = true;
+
+            StartCoroutine(BounceOn());
+        }
+    }
+
+    private IEnumerator BounceOn()
+    {
+        if (bounceable && !IsBouncing)
+        {
+            float bouncingAmount = 0.35f;
+
+            IsBouncing = true;
+
+            yield return StartCoroutine(Bounce(transform.position.y, transform.position.y + bouncingAmount));
+
+            yield return StartCoroutine(Bounce(transform.position.y, transform.position.y - bouncingAmount));
+
+            IsBouncing = false;
+
+            IsHit = false;
+        }
+    }
+
+    private IEnumerator Bounce(float startY, float endY)
+    {
+        float percent = 0;
+        float bouncingTime = 0.2f;
+
+        while ( percent < 1 )
+        {
+            percent += Time.deltaTime / bouncingTime;
+
+            float y = Mathf.Lerp(startY, endY, percent);
+            transform.position = new Vector3(transform.position.x, y, transform.position.z);
+
+            yield return null;
+        }
     }
 }
