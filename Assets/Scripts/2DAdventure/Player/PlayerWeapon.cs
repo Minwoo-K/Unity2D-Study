7c329d258883d74a20d6bce6c426ba073c300ccc
Minwoo Kim <minwook1130@gmail.com>
2024-04-16 13:40:01 -0400

    Added PlayerWeapon script

--- Assets/Scripts/2DAdventure/Player/PlayerWeapon.cs
+++ Assets/Scripts/2DAdventure/Player/PlayerWeapon.cs
@@ -4,5 +4,14 @@ using UnityEngine;
 
 public class PlayerWeapon : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject projectile;
+    [SerializeField]
+    private Transform spawningPoint;
 
+    public void FireProjectile(float direction)
+    {
+        GameObject clone = Instantiate(projectile, spawningPoint.position, Quaternion.identity);
+        clone.GetComponent<Projectile>().Setup(direction);
+    }
 }
