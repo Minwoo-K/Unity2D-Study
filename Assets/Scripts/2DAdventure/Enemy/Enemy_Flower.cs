91233bd6c921470f0cc698d9727eee4643f20d0c
Minwoo Kim <minwook1130@gmail.com>
2024-04-24 15:07:32 -0400

    Set up Flowers in scene

--- Assets/Scripts/2DAdventure/Enemy/Enemy_Flower.cs
+++ Assets/Scripts/2DAdventure/Enemy/Enemy_Flower.cs
@@ -5,11 +5,8 @@ using UnityEngine;
 public class Enemy_Flower : MonoBehaviour
 {
     [SerializeField]
-    private GameObject projectile;
-    [SerializeField]
     private float attackCoolTime;
-    
+
     private Animator animator;
-    private Vector2 projectileVelocity = new Vector2(0, -10);
 
 }
