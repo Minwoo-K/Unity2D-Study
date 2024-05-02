a7172159c2c77954bd161095da36f2522b318030
Minwoo Kim <minwook1130@gmail.com>
2024-05-02 14:11:09 -0400

    Added StageData scriptableObject

---
+++ Assets/Scripts/2DAdventure/StageData.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class StageData : ScriptableObject
+{
+    [Header("Player Limit")]
+    [SerializeField]
+    private float playerMinLimitX;
+    [SerializeField]
+    private float playerMaxLimitX;
+
+    [Header("Camera Limit")]
+    [SerializeField]
+    private float cameraMinLimitX;
+    [SerializeField]
+    private float cameraMaxLimitX;
+    [SerializeField]
+    private float cameraMaxLimitY;
+
+    [Header("Map")]
+    [SerializeField]
+    private float mapMinLimitY;
+
+    // Properties
+    public float PlayerMinLimitX => playerMinLimitX;
+    public float PlayerMaxLimitX => playerMaxLimitX;
+    public float CameraMinLimitX => cameraMinLimitX;
+    public float CameraMaxLimitX => cameraMaxLimitX;
+    public float CameraMaxLimitY => cameraMaxLimitY;
+    public float MapMinLimitY    => mapMinLimitY;
+}
