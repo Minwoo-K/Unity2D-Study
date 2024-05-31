a2d98ad2abe5d857339c68ae0b6feb2f8de294b5
Minwoo Kim <minwook1130@gmail.com>
2024-05-31 15:47:45 -0400

    Implemented Platform_Drop shaking

--- Assets/Scripts/2DAdventure/Platforms/Platform_Drop.cs
+++ Assets/Scripts/2DAdventure/Platforms/Platform_Drop.cs
@@ -4,8 +4,32 @@ using UnityEngine;
 
 public class Platform_Drop : Platform_Base
 {
+    [SerializeField]
+    private float fallingTime;
+    [SerializeField]
+    private bool respawn;
+
     public override void UpdateCollision(Transform player)
     {
-        
+        if ( player.CompareTag("Player") )
+        {
+            StartCoroutine(ShakeOn());
+        }
+    }
+
+    private IEnumerator ShakeOn()
+    {
+        float shakeTime = 3f, shakeAngle = 10f, shakeIntensity = 0.8f, shakeSpeed = 12f, percent = 0;
+
+        while ( percent < 1 )
+        {
+            percent += Time.deltaTime / shakeTime;
+
+            float z = Mathf.Lerp(-shakeAngle, shakeAngle, Mathf.PingPong(shakeSpeed * shakeIntensity * Time.time, 1));
+
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, z));
+
+            yield return null;
+        }
     }
 }
