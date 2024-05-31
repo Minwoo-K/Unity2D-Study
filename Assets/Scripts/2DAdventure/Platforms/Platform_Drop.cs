a9399d026935351e28df1852a9d707116ddfb56b
Minwoo Kim <minwook1130@gmail.com>
2024-05-31 15:19:22 -0400

    Created scripts for other Platforms
    - Added base to Platform_Drop prefab

---
+++ Assets/Scripts/2DAdventure/Platforms/Platform_Drop.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform_Drop : Platform_Base
+{
+    public override void UpdateCollision(Transform player)
+    {
+        
+    }
+}
