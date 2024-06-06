daaac2768f878c0d6991d53cfd02e20e51448fd3
Minwoo Kim <minwook1130@gmail.com>
2024-06-06 10:55:55 -0400

    Obstacle ChainAxe in progress

---
+++ Assets/Scripts/2DAdventure/Obstacles/Obstacle_ChainAxe.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle_ChainAxe : Obstacle_Base
+{
+    [SerializeField]
+    private Transform nut;
+    [SerializeField]
+    private float swingingDegree;
+}
