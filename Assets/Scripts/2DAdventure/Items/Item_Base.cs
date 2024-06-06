6f5c973b7b9ef57cfa7e4ef2516221f79453fbbf
Minwoo Kim <minwook1130@gmail.com>
2024-06-06 11:59:57 -0400

    Implemented Coin taken function

---
+++ Assets/Scripts/2DAdventure/Items/Item_Base.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Item_Base : MonoBehaviour
+{
+    public abstract void UpdateCollision(GameObject player);
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if ( collision.CompareTag("Player") )
+        {
+            UpdateCollision(collision.gameObject);
+        }
+    }
+}
