046d9dbd55434b50102ea0727ba5f8b5aec37148
Minwoo Kim <minwook1130@gmail.com>
2024-06-06 11:28:02 -0400

    Implemented Obstacle ChainAxe

--- Assets/Scripts/2DAdventure/Obstacles/Obstacle_ChainAxe.cs
+++ Assets/Scripts/2DAdventure/Obstacles/Obstacle_ChainAxe.cs
@@ -7,5 +7,14 @@ public class Obstacle_ChainAxe : Obstacle_Base
     [SerializeField]
     private Transform nut;
     [SerializeField]
-    private float swingingDegree;
+    private float swingingAngle;
+    [SerializeField]
+    private float swingingSpeed;
+
+    private void Update()
+    {
+        float z = swingingAngle * Mathf.Sin(Time.time * swingingSpeed);
+        
+        nut.rotation = Quaternion.Euler(new Vector3(0, 0, z));
+    }
 }
