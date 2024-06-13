d175aade4b1be715ee6102558a6e0715f9f37e2e
Minwoo Kim <minwook1130@gmail.com>
2024-06-13 14:46:57 -0400

    Added Projectile Count to PlayerWeapon script
    - Max Projectile Count is 9
    - Each Projectile item taken fills 3

--- Assets/Scripts/2DAdventure/Player/PlayerWeapon.cs
+++ Assets/Scripts/2DAdventure/Player/PlayerWeapon.cs
@@ -9,9 +9,22 @@ public class PlayerWeapon : MonoBehaviour
     [SerializeField]
     private Transform   spawningPoint;
 
+    private readonly int maxProjectileCount = 9;
+    [SerializeField]
+    private int          projectileCount = 0;
+
+    public void LoadProjectile()
+    {
+        projectileCount += 3;
+        if (projectileCount >= maxProjectileCount) projectileCount = maxProjectileCount;
+    }
+
     public void ShootProjectile(float direction)
     {
+        if ( projectileCount <= 0 ) return;
+
         GameObject projectile = Instantiate(projectilePrefab, spawningPoint.position, Quaternion.identity);
         projectile.GetComponent<PlayerProjectile>().Spawned(direction);
+        projectileCount--;
     }
 }
