45c6585adaa0d522af8c2f5ec126a8f3399a6a30
Minwoo Kim <minwook1130@gmail.com>
2024-08-15 15:29:47 -0400

    Implemented player's walking

--- Assets/Scripts/2DAdventure/MovementRigid2D.cs
+++ Assets/Scripts/2DAdventure/MovementRigid2D.cs
@@ -4,5 +4,36 @@ using UnityEngine;
 
 public class MovementRigid2D : MonoBehaviour
 {
-    
+    // SereializeFields
+    [SerializeField]
+    private LayerMask groundLayers;
+
+    [SerializeField]
+    private float walkSpeed = 8f;
+    [SerializeField]
+    private float runSpeed = 13f;
+
+    // Private Variables
+    private float moveSpeed;
+    private Rigidbody2D rigid2D;
+
+    // Properties
+    public bool IsOnGround { get; set; } = false;
+
+    private void Awake()
+    {
+        moveSpeed = walkSpeed;
+
+        rigid2D = GetComponent<Rigidbody2D>();
+    }
+
+    public void MoveTo(float input)
+    {
+        // if the input == 0.5f, walk. if == 1, run.
+        moveSpeed = Mathf.Abs(input) != 1 ? walkSpeed : runSpeed;
+
+        if ( input != 0 ) input = Mathf.Sign(input);
+
+        rigid2D.velocity = new Vector2(moveSpeed * input, rigid2D.velocity.y);
+    }
 }
--- Assets/Scripts/2DAdventure/PlayerController.cs
+++ Assets/Scripts/2DAdventure/PlayerController.cs
@@ -4,5 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    private MovementRigid2D movement;
+
+    private void Awake()
+    {
+        movement = GetComponent<MovementRigid2D>();
+    }
+
+    private void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float offset = 0.5f + Input.GetAxisRaw("Run") * 0.5f;
+        x *= offset;
+
+        MoveTo(x);
+    }
+
+    private void MoveTo(float input)
+    {
+        movement.MoveTo(input);
+    }
 }
