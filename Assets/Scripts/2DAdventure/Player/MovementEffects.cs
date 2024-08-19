cda398618f0792e8fb330190036c70157f1d1b6d
Minwoo Kim <minwook1130@gmail.com>
2024-08-19 15:30:20 -0400

    Added MovementParticleEffect to the player

--- Assets/Scripts/2DAdventure/Player/MovementEffects.cs
+++ Assets/Scripts/2DAdventure/Player/MovementEffects.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class MovementEffects : MonoBehaviour
 {
     [SerializeField]
+    private GameObject movementEffect;
+
+    private ParticleSystem movementParticle;
+    private ParticleSystem.EmissionModule emission;
+
+    [SerializeField]
     private GameObject landingEffect;
 
     private MovementRigid2D movement;
@@ -14,12 +20,23 @@ public class MovementEffects : MonoBehaviour
     private void Awake()
     {
         movement = GetComponent<MovementRigid2D>();
+        movementParticle = movementEffect.GetComponentInChildren<ParticleSystem>();
+        emission = movementParticle.emission;
     }
 
     private void Update()
     {
         if ( !wasOnGround && movement.IsOnGround ) 
             Instantiate(landingEffect, movement.FeetPosition, Quaternion.identity);
+
+        if ( movement.Velocity.x != 0 )
+        {
+            emission.rateOverTime = 30;
+        }
+        else
+        {
+            emission.rateOverTime = 0;
+        }
 
         wasOnGround = movement.IsOnGround;
     }
