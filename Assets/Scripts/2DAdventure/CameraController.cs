9dcd832bce366e290f9c7995b2fce2a6dda8a785
Minwoo Kim <minwook1130@gmail.com>
2024-09-06 15:59:19 -0400

    Implemented CameraController
    - With SmoothDamp

--- Assets/Scripts/2DAdventure/CameraController.cs
+++ Assets/Scripts/2DAdventure/CameraController.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private StageData stageData;
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private float smoothTime;
+    [SerializeField]
+    private bool x, y, z;
+
+    private float offsetY;
+    private Vector3 velocity = Vector3.zero;
+
+    private void Awake()
     {
-        
+        offsetY = transform.position.y - target.position.y;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
-        
+        Vector3 targetPosition = new Vector3(x ? target.position.x : transform.position.x,
+                                             y ? target.position.y + offsetY : transform.position.y,
+                                             z ? target.position.z - 10 : transform.position.z);
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+
+        float posX = transform.position.x;
+        posX = Mathf.Clamp(posX, stageData.CameraMinX, stageData.CameraMaxX);
+        float posY = transform.position.y;
+        posY = Mathf.Clamp(posY, stageData.MapMinY, transform.position.y);
+
+        transform.position = new Vector3(posX, posY, transform.position.z);
     }
 }
